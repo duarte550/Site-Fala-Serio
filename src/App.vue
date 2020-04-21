c9708289038bdab11af2b2d30ab9ea25f5b97ba8
duarte <duartegabriel550@gmail.com>
2020-04-21 03:49:55 -0300

    vue-cli installation

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div><navbar></navbar></div>
+</template>
+
+<script>
+
+import navbar from './components/navbar.vue';
+export default {
+  name: 'App',
+  components: {
+    navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="retangulo">
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'navbar',
+  
+}
+</script>
+
+<style>
+    .retangulo{
+        width: 500px;
+        background-color: green;
+    }
+</style>
